aaa7da0b5bfff04c854eb2f4b8e1be5f7e8d8693
Prateek Bajpai <44beastmodeon@Prateeks-MacBook-Pro.local>
2018-08-02 12:05:32 +0530

    stable commit

--- js/app.js
+++ js/app.js
@@ -1,44 +1,78 @@
 (function()
 {
-  angular.module('loginForms',[]).
-  controller('loginFormController',loginFormController).$inject=[];
-  function loginFormController()
+  angular.module('loginForms',['firebase']).
+  controller('loginFormController',loginFormController).$inject=['$firebase','$firebaseAuth'];
+  function loginFormController($firebase,$firebaseAuth)
   {
-
+    console.log('loginFormController');
     var loginFormCtrl=this;
     loginFormCtrl.value="0";
+    loginFormCtrl.firstname;
+    loginFormCtrl.lastname;
+    loginFormCtrl.msg;
+    loginFormCtrl.email;
+    var val=0;
     loginFormCtrl.flagFirstName="0";
     loginFormCtrl.flagLastName="0";
     loginFormCtrl.flagEmail="0";
+    loginFormCtrl.registerUserAPI=function()
+    {
+      var dbref=firebase.database().ref();
+      var auth=$firebaseAuth();
+      auth.$createUserWithEmailAndPassword(
+        loginFormCtrl.email,loginFormCtrl.email
+      ).then(function(registerUser)
+    {
+      loginFormCtrl.msg="Hi "+loginFormCtrl.firstname;
+       console.log(loginFormCtrl.msg);
+    }).catch(function error()
+  {
+    loginFormCtrl.msg="Something went wrong while registering";
+  });
+
+    };
+
+
     loginFormCtrl.nameValidator=function()
     {
-      if(loginFormCtrl.flagFirstName==="0")
+      if(loginFormCtrl.flagFirstName!==undefined && loginFormCtrl.flagFirstName==="0")
       {
         console.log('flagFirstName');
-        loginFormCtrl.value="25";
+        val=val+25;
+        loginFormCtrl.value=val;
         loginFormCtrl.flagFirstName="1";
       }
-      else if(loginFormCtrl.flagLastName==="0")
+      else if(loginFormCtrl.flagLastName!==undefined && loginFormCtrl.flagLastName==="0")
       {
         console.log('flagLastName');
-        loginFormCtrl.value="50";
+        val=val+25;
+        loginFormCtrl.value=val;
         loginFormCtrl.flagLastName="1";
       }
 
 
     };
+    loginFormCtrl.emailValidator()
+    {
+      
+    }
     loginFormCtrl.check=function()
     {
       console.log('NameValidator');
       var minnamelen=5;
-      if(loginFormCtrl.firstname.length>=5 && loginFormCtrl.flagFirstName==="0")
+      if(loginFormCtrl.firstname!==undefined && loginFormCtrl.firstname.length>=5 && loginFormCtrl.flagFirstName==="0")
       {
         loginFormCtrl.nameValidator();
 
       }
-      else if(loginFormCtrl.lastname.length>=5 && loginFormCtrl.flagLastName==="0")
+      else if(loginFormCtrl.lastname!==undefined && loginFormCtrl.lastname.length>=5 && loginFormCtrl.flagLastName==="0")
       {
         loginFormCtrl.nameValidator();
+      }
+      else if(loginFormCtrl.email.length>5 && loginFormCtrl.email.indexOf('@')>=0 )
+      {
+
+
       }
     };
 
